630e51492624d7458f271f3154e41b84fdb11dbd
pateldevang <devangpatel92@gmail.com>
2019-09-16 19:00:30 +0530

    rules view controller added

---
+++ Enigma/Controllers/RulesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RulesViewController.swift
+//  Enigma
+//
+//  Created by Devang Patel on 16/09/19.
+//  Copyright © 2019 IEEE-VIT. All rights reserved.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
