e48e3c4786b22b9e7573a931ca9eb4a7203ea1c1
pateldevang <devangpatel92@gmail.com>
2019-09-16 14:26:55 +0530

    Sign Up page

---
+++ Enigma/Controllers/signUpPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  signUpPageViewController.swift
+//  Enigma
+//
+//  Created by Devang Patel on 16/09/19.
+//  Copyright © 2019 IEEE-VIT. All rights reserved.
+//
+
+import UIKit
+
+class signUpPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
