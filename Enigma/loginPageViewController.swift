c1689fc6396fcc021286bcd316329f397a3485cc
pateldevang <devangpatel92@gmail.com>
2019-09-16 12:06:43 +0530

    login page done

---
+++ Enigma/loginPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  loginPageViewController.swift
+//  Enigma
+//
+//  Created by Devang Patel on 16/09/19.
+//  Copyright © 2019 IEEE-VIT. All rights reserved.
+//
+
+import UIKit
+
+class loginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
