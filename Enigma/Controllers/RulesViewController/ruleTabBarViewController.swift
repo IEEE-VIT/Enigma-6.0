48a8685c1342bee679c2ad1fc38d31141198f0fe
pateldevang <devangpatel92@gmail.com>
2019-09-20 22:47:03 +0530

    tab bar rule setup

---
+++ Enigma/Controllers/RulesViewController/ruleTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ruleTabBarViewController.swift
+//  Enigma
+//
+//  Created by Devang Patel on 20/09/19.
+//  Copyright © 2019 IEEE-VIT. All rights reserved.
+//
+
+import UIKit
+
+class ruleTabBarViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var rulesTable: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
