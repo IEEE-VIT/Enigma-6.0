30a68937b9ab1923f8b6b84407aa580a7b8f7036
pateldevang <devangpatel92@gmail.com>
2019-10-09 11:44:46 +0530

    corner radius button left done for key button

---
+++ Enigma/Extensions/RoundedCornerView.swift
@@ -0,0 +1,89 @@
+//
+//  RoundedCornerView.swift
+//  Enigma
+//
+//  Created by Devang Patel on 09/10/19.
+//  Copyright © 2019 IEEE-VIT. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedCornerView: UIButton {
+    
+    var cornerRadiusValue : CGFloat = 0
+    var corners : UIRectCorner = []
+    
+    @IBInspectable public var cornerRadius:CGFloat {
+        get {
+            return cornerRadiusValue
+        }
+        set {
+            cornerRadiusValue = newValue
+        }
+    }
+    
+    @IBInspectable public var topLeft : Bool {
+        get {
+            return corners.contains(.topLeft)
+        }
+        set {
+            setCorner(newValue: newValue, for: .topLeft)
+        }
+    }
+    
+    @IBInspectable public var topRight : Bool {
+        get {
+            return corners.contains(.topRight)
+        }
+        set {
+            setCorner(newValue: newValue, for: .topRight)
+        }
+    }
+    
+    @IBInspectable public var bottomLeft : Bool {
+        get {
+            return corners.contains(.bottomLeft)
+        }
+        set {
+            setCorner(newValue: newValue, for: .bottomLeft)
+        }
+    }
+    
+    @IBInspectable public var bottomRight : Bool {
+        get {
+            return corners.contains(.bottomRight)
+        }
+        set {
+            setCorner(newValue: newValue, for: .bottomRight)
+        }
+    }
+    
+    func setCorner(newValue: Bool, for corner: UIRectCorner) {
+        if newValue {
+            addRectCorner(corner: corner)
+        } else {
+            removeRectCorner(corner: corner)
+        }
+    }
+    
+    func addRectCorner(corner: UIRectCorner) {
+        corners.insert(corner)
+        updateCorners()
+    }
+    
+    func removeRectCorner(corner: UIRectCorner) {
+        if corners.contains(corner) {
+            corners.remove(corner)
+            updateCorners()
+        }
+    }
+    
+    func updateCorners() {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadiusValue, height: cornerRadiusValue))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+}
